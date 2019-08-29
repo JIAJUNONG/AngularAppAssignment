44af0bfb41ca81fcc5069f30afbddc75a4bb97af
JIAJUNONG <jia_jun_ong@dell.com>
2019-08-29 16:42:00 +0800

    added Bootstrap into Index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { HomeChildComponent } from '../app/home-child/home-child.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'home-child', component: HomeChildComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
