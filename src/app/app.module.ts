b4055846525aaf227f8ef89863c418d995e9764e
JIAJUNONG <jia_jun_ong@dell.com>
2019-08-30 13:09:27 +0800

    Completed HomePage and required some Chart Design to be done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { HomeChildComponent } from './home-child/home-child.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
 
 
 @Component({
@@ -8,6 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [15339, 21345, 18483, 24003, 23489, 24092, 12034], label: 'Series A' }
+  
+  ];
+  public lineChartLabels: Label[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartColors: Color[] = [
+
+  ];
 
   constructor() { }
 
