d6b71ad5a3aa48795eee6dd68a77aa1662525d20
JIAJUNONG <jia_jun_ong@dell.com>
2019-08-30 13:16:40 +0800

    Completed HomePage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,13 +12,17 @@ export class HomeComponent implements OnInit {
 
   public lineChartData: ChartDataSets[] = [
     { data: [15339, 21345, 18483, 24003, 23489, 24092, 12034], label: 'Series A' }
-  
+
   ];
   public lineChartLabels: Label[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   public lineChartLegend = true;
   public lineChartType = 'line';
   public lineChartColors: Color[] = [
-
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff'}
   ];
 
   constructor() { }
